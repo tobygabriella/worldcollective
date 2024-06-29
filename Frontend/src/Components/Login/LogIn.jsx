d79ae2acb409ed053dc6ba2d452829c43ac19ebe
Gabriella Gabriel Toby <tobygabriella@meta.com>
2024-06-28 19:41:02 -0700

    made username and password required

--- Frontend/src/Components/Login/LogIn.jsx
+++ Frontend/src/Components/Login/LogIn.jsx
@@ -56,14 +56,14 @@ const LogIn = () => {
             className="inputField"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-          />
+            required/>
           <input
             type="password"
             placeholder="Password"
             className="inputField"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          />
+            required/>
           {error && <p className="error">{error}</p>}
           <button type="submit" className="loginButton">Log in</button>
         </form>
