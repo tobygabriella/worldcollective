8146f6eb699024980cf0dd99c9e805229d743411
Gabriella Gabriel Toby <tobygabriella@meta.com>
2024-07-03 20:13:44 -0700

    minor changes to improve the UI

--- Frontend/src/Components/ProfileContent/ProfileContent.jsx
+++ Frontend/src/Components/ProfileContent/ProfileContent.jsx
@@ -5,32 +5,41 @@ import ListingsContainer from "../ListingsContainer/ListingsContainer";
 import "./ProfileContent.css";
 
 const ProfileContent = ({ user, title }) => {
-  return (
-    <div className="profileContainer">
-      <AppHeader />
-      <div className="profileInfo">
-        <img
-          src={user.profilePicture || "default-profile.png"}
-          alt="Profile"
-          className="profilePicture"
-        />
-        <div className="userInfo">
-          <h2>{user.name || "Name of User"}</h2>
-          <h4>@{user.username}</h4>
-          <p>Bio: {user.bio || "No bio available"}</p>
-        </div>
-      </div>
-      <div className="userListings">
-        <ListingsContainer
-          title={title}
-          listings={user.listings}
-          showFilters={false}
-        >
-          {(listing) => <ListingItem key={listing.id} {...listing} />}
-        </ListingsContainer>
-      </div>
-    </div>
-  );
+
+      const getInitials = (firstName, lastName) => {
+        if (!firstName && !lastName) return "";
+        return `${firstName[0] || ""}${lastName[0] || ""}`.toUpperCase();
+      };
+
+    const initials = getInitials(user.firstname, user.lastname);
+
+ return (
+   <div className="profileContainer">
+     <AppHeader />
+     <div className="profileInfo">
+       <div className="profilePicture">
+         {user.profilePicture ? (
+           <img src={user.profilePicture} alt="Profile" />
+         ) : (
+           <div className="initials">{initials}</div>
+         )}
+       </div>
+       <div className="userInfo">
+         <h2>{`${user.firstname} ${user.lastname}` || "Name of User"}</h2>
+         <h4>@{user.username}</h4>
+       </div>
+     </div>
+     <div className="userListings">
+       <ListingsContainer
+         title={title}
+         listings={user.listings}
+         showFilters={false}
+       >
+         {(listing) => <ListingItem key={listing.id} {...listing} />}
+       </ListingsContainer>
+     </div>
+   </div>
+ );
 };
 
 export default ProfileContent;
