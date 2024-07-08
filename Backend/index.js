cc0be31ab6f7d113c03e56a6b59137d0321e94ce
Gabriella Gabriel Toby <tobygabriella@meta.com>
2024-07-08 10:42:45 -0700

    installing socket io and setting up the server in backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const cors = require("cors");
@@ -17,6 +19,13 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const saltRounds = 14;
 const secretKey = process.env.JWT_SECRET;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
 const port = 3002;
 
 app.use(bodyParser.json());
@@ -30,6 +39,14 @@ app.use(
     credentials: true,
   })
 );
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
