9a753e873bf59a4cd0a91591279cce3068f9aece
Gabriella Gabriel Toby <tobygabriella@meta.com>
2024-07-09 12:44:26 -0700

    quick fixes

--- Frontend/src/Components/Notifications/Notifications.jsx
+++ Frontend/src/Components/Notifications/Notifications.jsx
@@ -4,12 +4,13 @@ import Loading from "../Loading/Loading.jsx";
 import useLoading from "../CustomHooks/useLoading.jsx";
 import "./Notifications.css";
 import AppHeader from "../Headers/AppHeader";
+import { useNavigate } from "react-router-dom";
 
-const API_KEY = import.meta.env.VITE_BACKEND_ADDRESS;
 
 const Notifications = () => {
   const { notifications, markAsRead } = useSocket();
   const { isLoading, stopLoading } = useLoading();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (notifications.length > 0) {
@@ -29,6 +30,18 @@ const Notifications = () => {
     return <Loading />;
   }
 
+  const handleNotificationClick = (notif) => {
+    if (notif.type === "FOLLOW") {
+      navigate(`/users/${notif.usernameTarget}`);
+    } else if (
+      notif.type === "LIKE" ||
+      notif.type === "PURCHASE" ||
+      notif.type === "LIKE_PURCHASE"
+    ) {
+      navigate(`/listings/${notif.listingId}`);
+    }
+  };
+
   return (
     <div className="notificationsPage">
       <AppHeader />
@@ -41,9 +54,24 @@ const Notifications = () => {
             <li
               key={index}
               className={`notificationItem ${notif.isRead ? "read" : "unread"}`}
+              onClick={() => handleNotificationClick(notif)}
             >
               <div className="notificationContent">
                 {!notif.isRead && <span className="unreadIndicator"></span>}
+                {(notif.type === "LIKE" ||
+                  notif.type === "LIKE_PURCHASE" ||
+                  notif.type === "PURCHASE") &&
+                  notif.listingImage && (
+                    <img
+                      src={notif.listingImage}
+                      alt="Listing"
+                      className="notificationImage"
+                    />
+                  )}
+                {notif.type === "FOLLOW" && (
+                  <div className="profileCircle">
+                  </div>
+                )}
                 <p>{notif.content}</p>
               </div>
               <span className="notificationTime">{notif.timeAgo}</span>
