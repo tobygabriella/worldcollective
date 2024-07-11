06c6da27534a5c2f2b3c58b11bc048005902cb3b
Gabriella Gabriel Toby <tobygabriella@meta.com>
2024-07-10 21:54:17 -0700

    add a leave a review button after successful payment

--- Frontend/src/Components/Checkout/ConfirmationPage.jsx
+++ Frontend/src/Components/Checkout/ConfirmationPage.jsx
@@ -1,13 +1,49 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import AppHeader from "../Headers/AppHeader";
+import ReviewModal from "../Review/CreateReviewModal";
 import "./ConfirmationPage.css";
+
+const API_KEY = import.meta.env.VITE_BACKEND_ADDRESS;
 
 const ConfirmationPage = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { listingId, sellerId } = location.state || {};
+  const [isReviewModalOpen, setIsReviewModalOpen] = useState(false);
 
   const handleBackToListings = () => {
     navigate("/userProfile");
+  };
+
+  const handleLeaveReview = () => {
+    setIsReviewModalOpen(true);
+  };
+
+  const handleReviewSubmit = async (
+    review,
+    setSuccessMessage,
+    setErrorMessage
+  ) => {
+    try {
+      const response = await fetch(`${API_KEY}/listings/${listingId}/reviews`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({ ...review, sellerId }),
+      });
+
+      if (response.ok) {
+        setSuccessMessage("Review submitted successfully");
+      } else {
+        setErrorMessage("Failed to submit review");
+      }
+    } catch (error) {
+      setErrorMessage("Error submitting review");
+    }
   };
 
   return (
@@ -17,7 +53,13 @@ const ConfirmationPage = () => {
         <h2>Thank you for your purchase!</h2>
         <p>Your transaction was successful.</p>
         <button onClick={handleBackToListings}>Back to your profile</button>
+        <button onClick={handleLeaveReview}>Leave a Review</button>
       </div>
+      <ReviewModal
+        isOpen={isReviewModalOpen}
+        onClose={() => setIsReviewModalOpen(false)}
+        onSubmit={handleReviewSubmit}
+      />
     </div>
   );
 };
