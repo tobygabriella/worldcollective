324f41246948ae10561dedbfa8e8d159035fcae6
Gabriella Gabriel Toby <tobygabriella@meta.com>
2024-07-22 20:31:21 -0700

    users can check out an item from cart and add an item to cart

--- Frontend/src/Components/Checkout/PaymentPage.jsx
+++ Frontend/src/Components/Checkout/PaymentPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import AppHeader from "../Headers/AppHeader";
 import Loading from "../Loading/Loading.jsx";
@@ -9,45 +9,19 @@ import "./PaymentPage.css";
 const API_KEY = import.meta.env.VITE_BACKEND_ADDRESS;
 
 const PaymentPage = () => {
-  const { listing } = useOutletContext();
   const stripe = useStripe();
   const elements = useElements();
   const navigate = useNavigate();
-  const [clientSecret, setClientSecret] = useState("");
+  const location = useLocation();
+  const { clientSecret, listing, cartItems } = location.state;
   const { isLoading } = useLoading();
   const [success, setSuccess] = useState(false);
 
   useEffect(() => {
-    if (listing) {
-      const createPaymentIntent = async () => {
-        try {
-          const paymentResponse = await fetch(
-            `${API_KEY}/create-payment-intent`,
-            {
-              method: "POST",
-              credentials: "include",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-              },
-              body: JSON.stringify({ amount: listing.price }),
-            }
-          );
-
-          if (!paymentResponse.ok) {
-            throw new Error("Network response for payment intent was not ok");
-          }
-
-          const paymentData = await paymentResponse.json();
-          setClientSecret(paymentData.clientSecret);
-        } catch (error) {
-          console.error("Error creating payment intent:", error);
-        }
-      };
-
-      createPaymentIntent();
+    if (!clientSecret || (!listing && !cartItems)) {
+      navigate("/");
     }
-  }, [listing]);
+  }, [clientSecret, listing, cartItems, navigate]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -76,26 +50,59 @@ const PaymentPage = () => {
     } else {
       if (paymentIntent.status === "succeeded") {
         try {
-          const response = await fetch(
-            `${API_KEY}/listings/${listing.id}/complete-purchase`,
-            {
+          if (listing) {
+            const response = await fetch(
+              `${API_KEY}/listings/${listing.id}/complete-purchase`,
+              {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+                body: JSON.stringify({ paymentIntentId: paymentIntent.id }),
+              }
+            );
+
+            if (response.ok) {
+              setSuccess(true);
+              navigate("/confirmation", {
+                state: { listingId: listing.id, sellerId: listing.sellerId },
+                isSingleCheckout: true,
+              });
+            } else {
+              console.error(
+                "Failed to update transaction status on the server"
+              );
+            }
+          } else if (cartItems) {
+            const listingIds = cartItems.map((item) => ({
+              id: item.id,
+              sellerId: item.sellerId,
+            }));
+
+            const response = await fetch(`${API_KEY}/cart/complete-purchase`, {
               method: "POST",
               credentials: "include",
               headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${localStorage.getItem("token")}`,
               },
-              body: JSON.stringify({ paymentIntentId: paymentIntent.id }),
-            }
-          );
-
-          if (response.ok) {
-            setSuccess(true);
-            navigate("/confirmation", {
-              state: { listingId: listing.id, sellerId: listing.sellerId },
+              body: JSON.stringify({
+                paymentIntentId: paymentIntent.id,
+              }),
             });
-          } else {
-            console.error("Failed to update transaction status on the server");
+
+            if (response.ok) {
+              setSuccess(true);
+              navigate("/confirmation", {
+                state: { listings: data.listings, isSingleCheckout: false },
+              });
+            } else {
+              console.error(
+                "Failed to update transaction status on the server"
+              );
+            }
           }
         } catch (error) {
           console.error(
@@ -126,11 +133,26 @@ const PaymentPage = () => {
                 Pay ${listing.price}
               </button>
             </form>
-            {success && <p>Payment successful! Redirecting...</p>}
           </>
         ) : (
-          <p>Listing not found.</p>
+          <>
+            <h2>Complete your purchase</h2>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  {item.title} - ${item.price}
+                </li>
+              ))}
+            </ul>
+            <form onSubmit={handleSubmit}>
+              <CardElement />
+              <button type="submit" disabled={!stripe}>
+                Pay
+              </button>
+            </form>
+          </>
         )}
+        {success && <p>Payment successful! Redirecting...</p>}
       </div>
     </div>
   );
